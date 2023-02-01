9c15b8ff93b5be8752abae5e9979d8ccfa4b50e6
benmamogus <misternini1@gmail.com>
2023-01-31 19:00:07 -0500

    added menu for real

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MenuScript : MonoBehaviour
 {
+
+    public void StartGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
