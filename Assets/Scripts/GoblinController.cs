36e5d622c56567be0193d58bf91c3310ddb03e55
Jackson Kenneth Clayton <ravagezsdev@gmail.com>
2023-01-31 19:07:49 -0500

    prefab changes

--- Assets/Scripts/GoblinController.cs
+++ Assets/Scripts/GoblinController.cs
@@ -6,16 +6,27 @@ public class GoblinController : MonoBehaviour
 {
     public GameObject cameraDock;
     public bool onGround;
+    Vector3 lastPos;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
+    private void FixedUpdate()
+    {
 
+        lastPos.y = transform.position.y;
+        float animSpeedTime = Vector3.Distance(transform.position, lastPos)/Time.fixedDeltaTime;
+        if (!onGround)
+            animSpeedTime /= 5;
+        GetComponent<Animator>().speed = animSpeedTime/10;
+lastPos = transform.position;
+    }
     // Update is called once per frame
     void Update()
     {
+
         onGround = Physics.Raycast(transform.position, Vector3.down, 1.6f);
         transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
         cameraDock.transform.Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
@@ -25,7 +36,7 @@ public class GoblinController : MonoBehaviour
         vel.y = GetComponent<Rigidbody>().velocity.y;
         if (Input.GetKey(KeyCode.Space) && onGround)
         {
-            vel.y = 10;
+            vel.y = 15;
         }
             GetComponent<Rigidbody>().velocity = vel;
     }
